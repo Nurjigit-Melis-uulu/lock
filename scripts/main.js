42ac9444822e32970d25f6133a60f96f8ece0b78
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-06-18 10:07:46 +0600

    fix animations

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,15 @@ let lock_ring = document.querySelector(".lock .ring");
 let lock_hanger = document.querySelector(".hanger");
 
 button_auth.onclick = () => {
-  lock_ring.style.animation = "unlock 2s 1 linear";
+  lock_ring.style.animation = "unlock 4s 1 linear";
 
   let animation = setTimeout(() => {
     clearTimeout(animation);
     lock_hanger.style.transform = "translateY(-100px)";
-  }, 2000);
+  }, 4000);
 
   let cleared = setTimeout(() => {
     lock_ring.style.animation = "none";
     lock_hanger.style.transform = "translateY(0)";
-  }, 3000);
+  }, 5000);
 };
